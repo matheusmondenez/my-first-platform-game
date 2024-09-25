33a4ebf94459323eb8ef54202060b7da348f6f8c
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-09-25 18:48:19 -0300

    fix: fixes jump animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,14 +6,7 @@ const JUMP_VELOCITY = -900.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
-	if velocity.x > 1 || velocity.x < -1:
-		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "idle"
-
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		sprite_2d.animation = "jumping"
+	set_animation(delta)
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -26,7 +19,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-  
-	var is_left = velocity.x < 0
-	
-	sprite_2d.flip_h = is_left
+	set_face_direction(direction)
+
+func set_animation(delta):
+	if is_on_floor():
+		if velocity.x > 1 || velocity.x < -1:
+			sprite_2d.animation = "running"
+		else:
+			sprite_2d.animation = "idle"
+	else:
+		velocity += get_gravity() * delta
+		sprite_2d.animation = "jumping"
+
+func set_face_direction(direction):
+	if (direction < 0):
+		sprite_2d.flip_h = true
+	elif (direction > 0):
+		sprite_2d.flip_h = false
