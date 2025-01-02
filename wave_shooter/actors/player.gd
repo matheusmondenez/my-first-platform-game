0af6c358193b5fd6076680c5057cee08cedf3890
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-02 20:20:20 -0300

    creates player movement

--- wave_shooter/actors/player.gd
+++ wave_shooter/actors/player.gd
@@ -1,9 +1,13 @@
 extends Polygon2D
 
 var stats: BaseStats
+var speed: int = 500
+var move: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	move.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	move.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	global_position += speed * move * delta
