bc7f2d8f332173c5902c2d8058a102fd636ce9fd
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-05 13:17:22 -0300

    creates player death

--- wave_shooter/actors/player.gd
+++ wave_shooter/actors/player.gd
@@ -4,7 +4,8 @@ var stats: BaseStats
 var speed: int = 500
 var move: Vector2 = Vector2.ZERO
 var projectile_scene: PackedScene = preload("res://wave_shooter/actors/projectile.tscn")
-var loaded: bool = true
+var is_loaded: bool = true
+var is_dead: bool = false
 
 func _ready() -> void:
 	Global.player = self
@@ -12,14 +13,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	move.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	move.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-	global_position += speed * move * delta
-	if Input.is_action_pressed("shoot") and Global.parent_node_creation and loaded:
+	if not is_dead:
+		global_position += speed * move * delta
+	if Input.is_action_pressed("shoot") and Global.parent_node_creation and is_loaded and not is_dead:
 		Global.instance_node(projectile_scene, global_position, Global.parent_node_creation)
-		loaded = false
+		is_loaded = false
 		$Timer.start()
 
 func _on_timer_timeout() -> void:
-	loaded = true
+	is_loaded = true
 
 func _exit_tree() -> void:
 	Global.player = null
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		is_dead = true
+		visible = false
+		await get_tree().create_timer(1.0).timeout
+		get_tree().reload_current_scene()
