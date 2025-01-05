66df00a27ff990de1e7a6bc7e0adf36d87ef55e6
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-05 13:08:05 -0300

    creates random enemy spawn

--- wave_shooter/levels/arena.gd
+++ wave_shooter/levels/arena.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+var enemy_one: PackedScene = preload("res://wave_shooter/actors/enemy.tscn")
+
 func _ready() -> void:
 	Global.parent_node_creation = self
 
 func _exit_tree() -> void:
 	Global.parent_node_creation = null
+
+func _on_enemy_spawn_timer_timeout() -> void:
+	var enemy_position = Vector2(randi_range(-160, 670), randi_range(-90, 390))
+	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(randi_range(-160, 670), randi_range(-90, 390))
+	Global.instance_node(enemy_one, enemy_position, self)
