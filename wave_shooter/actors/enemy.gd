1cc0bb366bb4e1ace150721d81ca5c567eb42e1b
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-05 14:48:35 -0300

    creates enemy blood scene

--- wave_shooter/actors/enemy.gd
+++ wave_shooter/actors/enemy.gd
@@ -19,7 +19,7 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("damage"):
+	if area.is_in_group("damage") and not is_stunned:
 		hp -= 1
 		is_stunned = true
 		#modulate = Color.WHITE
