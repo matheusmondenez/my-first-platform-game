efae204712f9ca5d3bd7d83d7daef8501425fd50
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-06 17:40:04 -0300

    creates power up scene and spawn

--- wave_shooter/levels/arena.gd
+++ wave_shooter/levels/arena.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var enemy_one: PackedScene = preload("res://wave_shooter/actors/enemy.tscn")
 var enemies = [
 	preload("res://wave_shooter/actors/enemy.tscn"),
 	preload("res://wave_shooter/actors/speedy_enemy.tscn"),
 ]
+var power_ups = [preload("res://wave_shooter/actors/power_up.tscn")]
 
 func _ready() -> void:
 	Global.parent_node_creation = self
@@ -17,9 +17,14 @@ func _on_enemy_spawn_timer_timeout() -> void:
 	var enemy_position = Vector2(randi_range(-160, 670), randi_range(-90, 390))
 	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
 		enemy_position = Vector2(randi_range(-160, 670), randi_range(-90, 390))
-	var enemy = round(randi_range(0, enemies.size() - 1))
-	Global.instance_node(enemies[enemy], enemy_position, self)
+	var enemy_index = round(randi_range(0, enemies.size() - 1))
+	Global.instance_node(enemies[enemy_index], enemy_position, self)
 
 func _on_dificulty_timer_timeout() -> void:
 	if $EnemySpawnTimer.wait_time > 0.5:
 		$DificultyTimer.wait_time -= 0.10
+
+func _on_power_up_spawn_timer_timeout() -> void:
+	var power_up_position = Vector2(randi_range(0, 1152), randi_range(0, 648))
+	var power_up_index = round(randi_range(0, power_ups.size() - 1))
+	Global.instance_node(power_ups[power_up_index], power_up_position, self)
