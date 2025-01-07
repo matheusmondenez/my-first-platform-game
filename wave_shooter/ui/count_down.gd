f15449079af8c36c445ea07f2d8ad08bf9987797
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-07 16:09:28 -0300

    refactores countdown script

--- wave_shooter/ui/count_down.gd
+++ wave_shooter/ui/count_down.gd
@@ -1,11 +1,20 @@
 extends Control
 
+var time_left: int = 5
+
 func _ready() -> void:
 	get_tree().paused = true
 
 func _process(delta: float) -> void:
-	$Counter.text = str("%01d" % $Timer.time_left)
+	pass
+	#$Counter.text = str("%01d" % $Timer.time_left)
+	#var tween: Tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT).set_loops()
+	#tween.tween_property($Counter, "modulate:a", 0, 0.5)
+	#tween.tween_property($Counter, "modulate:a", 1, 0.1)
 
 func _on_timer_timeout() -> void:
-	get_tree().paused = false
-	queue_free()
+	time_left -= 1
+	$Counter.text = str("%1d" % time_left)
+	if time_left < 0:
+		get_tree().paused = false
+		queue_free()
