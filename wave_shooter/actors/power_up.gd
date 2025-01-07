191545aff734165da84913cb67dbcd87e9c7b39d
Matheus Mondenez <matheus.paulino@kroton.com.br>
2025-01-07 11:51:35 -0300

    creates shield power up

--- wave_shooter/actors/power_up.gd
+++ wave_shooter/actors/power_up.gd
@@ -1,8 +1,11 @@
 extends Polygon2D
 
+var shield_scene: PackedScene = preload("res://wave_shooter/actors/shield.tscn")
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		Global.player.powered_up.append("green")
+		Global.player.add_child(shield_scene.instantiate())
 		queue_free()
 
 func _on_timer_timeout() -> void:
