403f99e7a03aee7771ab3a95c472a982d168a7e2
Matheus Mondenez <matheusmondenez@gmail.com>
2025-02-18 17:21:55 -0300

    implements pause

--- wave_shooter/ui/pause.gd
+++ wave_shooter/ui/pause.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	visible = false
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause") && get_tree().paused == false:
 		pause()
