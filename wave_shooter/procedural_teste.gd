743ba9391d4361e0a352f3badd563c2da4e74998
Matheus Mondenez <matheusmondenez@gmail.com>
2025-02-26 10:39:14 -0300

    fills map rect with tiles

--- wave_shooter/procedural_teste.gd
+++ wave_shooter/procedural_teste.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var Map: TileMapLayer = $TileMapLayer
+
 var Room = preload("res://wave_shooter/teste.tscn")
 var tile_size = 32
 var num_rooms = 50
@@ -33,6 +35,18 @@ func make_rooms():
 	await get_tree().process_frame
 	path = find_minimum_spanning_tree(room_positions)
 
+func make_map():
+	Map.clear()
+	var full_rect = Rect2()
+	for room in $Rooms.get_children():
+		var r = Rect2(room.position - room.size, room.get_node("Shape").shape.extents * 2)
+		full_rect = full_rect.merge(r)
+	var topleft = Map.local_to_map(full_rect.position)
+	var bottomright = Map.local_to_map(full_rect.end)
+	for x in range(topleft.x, bottomright.x):
+		for y in range(topleft.y, bottomright.y):
+			Map.set_cell(Vector2i(x, y), 1, Vector2i(13, 3), 0)
+
 func _draw():
 	for room in $Rooms.get_children():
 		draw_rect(Rect2(room.position - room.size, room.size * 2), Color(32, 228, 0), false)
@@ -51,6 +65,8 @@ func _input(event: InputEvent):
 		for n in $Rooms.get_children():
 			n.queue_free()
 		make_rooms()
+	if event.is_action_pressed("shoot"):
+		make_map()
 
 func find_minimum_spanning_tree(nodes):
 	var path = AStar2D.new()
