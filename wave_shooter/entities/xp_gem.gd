802f388a6aa988bccd95d5d7757f62001a15e10c
Matheus Mondenez <matheusmondenez@gmail.com>
2025-03-09 16:52:12 -0300

    creates xp gem magnet effect

--- wave_shooter/entities/xp_gem.gd
+++ wave_shooter/entities/xp_gem.gd
@@ -1,7 +1,15 @@
 extends Sprite2D
 
+var speed: int = 0
+
 func _on_area_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		#Global.xp += stats.given_xp
+		speed = 10
 		Global.xp += 1
+		await get_tree().create_timer(1).timeout
 		queue_free()
+
+func _process(delta: float) -> void:
+	var direction = global_position.direction_to(Global.player.global_position)
+	global_position += direction * speed
