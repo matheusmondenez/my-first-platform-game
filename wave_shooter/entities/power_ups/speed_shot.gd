77c59479637a0a242a3399fab4cbe55b8e589e98
Matheus Mondenez <matheusmondenez@gmail.com>
2025-03-09 16:51:48 -0300

    fixes timer node name

--- wave_shooter/entities/power_ups/speed_shot.gd
+++ wave_shooter/entities/power_ups/speed_shot.gd
@@ -3,7 +3,7 @@ extends Label
 @export var props: BasePowerUp
 
 func _ready() -> void:
-	Global.player.get_node("Timer").wait_time = 0.1
+	Global.player.get_node("ShotInterval").wait_time = 0.1
 	await get_tree().create_timer(props.duration).timeout
-	Global.player.get_node("Timer").wait_time = 0.2
+	Global.player.get_node("ShotInterval").wait_time = 0.2
 	queue_free()
