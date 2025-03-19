3d9d064c5b7d02499a772a018ec7e503dd1000d7
Matheus Mondenez <matheusmondenez@gmail.com>
2025-03-19 18:04:31 -0300

    creates new passive skills resources

--- wave_shooter/resoures/skills/base_skill.gd
+++ wave_shooter/resoures/skills/base_skill.gd
@@ -1,7 +1,8 @@
 class_name BaseSkill extends Resource
 
-@export var name: String
 @export var icon: Texture2D
+@export var name: String
+@export_multiline var description: String
 @export_enum("Attack", "Defense", "Passive") var type: String
 @export var power: int
 @export var cooldown: float
